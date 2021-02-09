305c41c39b0c60375ea7950912b37f3070416bc5
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-02-09 14:30:56 +0100

    Exo-186-PHP

---
+++ classes/VODService.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class VODService
+{
+    private string $name;
+    private string $price;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): VODService
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price): VODService
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+}
